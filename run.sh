ccf1953b12c946c94bedf5c80cdaebf68fdb6a25
Heather Han <heather@atomwise.com>
2023-02-27 13:21:56 -0800

    fix run

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 pyflyte run example.py training_workflow --hyperparameters '{"C": 0.1}'
 
 # running flyte workflows in a flyte cluster
-flytectl demo starpyflyte run --remote example.py training_workflow \
+flytectl demo start
+pyflyte run --remote example.py training_workflow \
     --hyperparameters '{"C": 0.1}'t
